dc6dcc8057f4c7ea097c8abca8f62227ac2eb9eb
Chrysostomos Koumides <chyrkoum@gmail.com>
2024-12-12 14:04:00 +0200

    Add FAQ and Testimonials data, implement responsive styles

---
+++ data/reviews/testimonials.js
@@ -0,0 +1,25 @@
+const testimonials = [
+  {
+    stars: 5,
+    review:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare.",
+    name: "John Doe",
+    position: "CEO, Tech Solutions",
+  },
+  {
+    stars: 5,
+    review:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare.",
+    name: "Jane Smith",
+    position: "CTO, Innovate Inc.",
+  },
+  {
+    stars: 5,
+    review:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare.",
+    name: "Michael Brown",
+    position: "Founder, StartUp Hub",
+  },
+];
+
+module.exports = testimonials;
